bf6156056564e380c1607a1d016331b57f313a83
Guilherme Silva <guilherme.silva@dtidigital.com.br>
2025-10-02 08:59:27 -0300

    fix: disable auto-reloadt

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     host="0.0.0.0",
     port=8000,
     workers=1,
-    reload=True, # Ativar recarga para desenvolvimento
+    reload=False, # Ativar recarga para desenvolvimento
   )
